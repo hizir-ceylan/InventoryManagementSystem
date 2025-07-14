40ae936e612e05bd70b9346dbd8dcf38a8d002a6
Hızır Ceylan <ceylanhizir53@gmail.com>
2025-07-14 10:39:27 +0300

    .

--- Inventory.Domain/Entities/DeviceHardwareInfo.cs
+++ Inventory.Domain/Entities/DeviceHardwareInfo.cs
@@ -1,11 +1,53 @@
-﻿namespace Inventory.Domain.Entities
+﻿using System.Collections.Generic;
+
+namespace Inventory.Domain.Entities
 {
     public class DeviceHardwareInfo
     {
         public string Cpu { get; set; }
-        public int RamGB { get; set; }
-        public int DiskGB { get; set; }
+        public int CpuCores { get; set; }
+        public int CpuLogical { get; set; }
+        public int CpuClockMHz { get; set; }
         public string Motherboard { get; set; }
-        public string Gpu { get; set; }
+        public string MotherboardSerial { get; set; }
+        public string BiosManufacturer { get; set; }
+        public string BiosVersion { get; set; }
+        public string BiosSerial { get; set; }
+        public int RamGB { get; set; }
+        public List<RamModule> RamModules { get; set; }
+        public int DiskGB { get; set; }
+        public List<DiskInfo> Disks { get; set; }
+        public List<GpuInfo> Gpus { get; set; }
+        public List<NetworkAdapter> NetworkAdapters { get; set; }
+    }
+
+    public class RamModule
+    {
+        public string Slot { get; set; }
+        public double CapacityGB { get; set; }
+        public string SpeedMHz { get; set; }
+        public string Manufacturer { get; set; }
+        public string PartNumber { get; set; }
+        public string SerialNumber { get; set; }
+    }
+
+    public class DiskInfo
+    {
+        public string DeviceId { get; set; }
+        public double TotalGB { get; set; }
+        public double FreeGB { get; set; }
+    }
+
+    public class GpuInfo
+    {
+        public string Name { get; set; }
+        public float? MemoryGB { get; set; }
+    }
+
+    public class NetworkAdapter
+    {
+        public string Description { get; set; }
+        public string MacAddress { get; set; }
+        public string IpAddress { get; set; }
     }
 }
--- Inventory.Domain/Entities/DeviceSoftwareInfo.cs
+++ Inventory.Domain/Entities/DeviceSoftwareInfo.cs
@@ -1,12 +1,17 @@
-﻿namespace Inventory.Domain.Entities
+﻿using System.Collections.Generic;
+
+namespace Inventory.Domain.Entities
 {
     public class DeviceSoftwareInfo
     {
         public string OperatingSystem { get; set; }
         public string OsVersion { get; set; }
-        public string[] InstalledApps { get; set; }
-        public string[] Updates { get; set; }
-        public string[] Users { get; set; }
+        public string OsArchitecture { get; set; }
+        public string RegisteredUser { get; set; }
+        public string SerialNumber { get; set; }
+        public List<string> InstalledApps { get; set; }
+        public List<string> Updates { get; set; }
+        public List<string> Users { get; set; }
         public string ActiveUser { get; set; }
     }
 }
