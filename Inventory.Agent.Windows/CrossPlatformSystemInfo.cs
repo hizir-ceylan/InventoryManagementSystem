7a7670cb9e4ee855e6e5a2d49ad97d7632996401
Hızır Ceylan <ceylanhizir53@gmail.com>
2025-08-04 11:22:16 +0300

    software limit

--- Inventory.Agent.Windows/CrossPlatformSystemInfo.cs
+++ Inventory.Agent.Windows/CrossPlatformSystemInfo.cs
@@ -386,7 +386,7 @@ namespace Inventory.Agent.Windows
         private static List<string> GetInstalledSoftwareFast()
         {
             var installedApps = new List<string>();
-            const int maxApps = 150; // Limit to improve performance
+            const int maxApps = 300; // Limit to improve performance
 
             try
             {
